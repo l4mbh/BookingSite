4e94bef1a81993237a7bafe1119a15056629e018
lambh <lambh.1998@gmail.com>
2024-02-24 00:56:33 +0700

    Fix CORS

---
+++ back-end/api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+// Routers
+const hotelRoutes = require('./routers/hotel');
+const userRoutes = require('./routers/user');
+const adminRoutes = require('./routers/admin');
+
+const app = express();
+
+app.use(cors({
+  origin: ["https://booking-site-server-psi.vercel.app"],
+  methods: ["GET,OPTIONS,PATCH,DELETE,POST,PUT"],
+  credentials: true
+}));
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// Use routes
+app.use('/hotel', hotelRoutes);
+app.use('/user', userRoutes);
+app.use('/admin', adminRoutes);
+
+app.use("/", (req, res, next) => {
+  res.send("Hello")
+})
+mongoose.connect('mongodb+srv://lambh1998:25076301a@cluster0.kyjcau4.mongodb.net/booking');
